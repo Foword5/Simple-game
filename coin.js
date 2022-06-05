af196e917cabdaf4fabc90e6dbb9c7a8234c8862
Jules Delamare <julesd2509@gmail.com>
2022-06-05 23:52:28 +0200

    created coin class

---
+++ coin.js
@@ -0,0 +1,31 @@
+class coin {
+    constructor(){
+        this.x = getRandomInt(680)+50;
+        this.y = getRandomInt(680)+50;
+        this.size = 20;
+    }
+
+    touch_move_coin(main_char){
+        if (
+        main_char.getX() < this.x + this.size && 
+        main_char.getX() + main_char.getSize() > this.x && 
+        main_char.getY() < this.y + this.size && 
+        main_char.getSize() + main_char.getY() > this.y) {
+    
+            drawcube(this.x,this.y,this.size,"darkgrey");
+    
+            this.x = getRandomInt(680)+50;
+            this.y = getRandomInt(680)+50;
+
+            increment_score(main_char)
+        }
+    }
+
+    draw(score){
+        if([4,9,19,49,99,199,299,399,499,999].includes(score)){
+            drawImage(coin_reverse_image,this.x,this.y)
+        }else{
+            drawImage(coin_image,this.x,this.y)
+        }
+    }
+}
